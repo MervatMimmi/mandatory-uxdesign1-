be1df8b5202b9dd3820e93df4e377f649201408e
Mervat Hassan <mervat.hassan@gmail.com>
2020-03-24 15:41:06 +0100

    last change

--- src/Components/TextField.js
+++ src/Components/TextField.js
@@ -3,33 +3,13 @@ import './TextField.scss';
 
 export default function TextField() {
     const [inputValue, updateInputValue] = useState('');
-    //const [fieldActive, updateFieldActive] = useState(false);
-    
-    /*function disableFocus(e) {
-        if(e.target.value === '') {
-            updateFieldActive(false)
-        }
-    }
-
-   /* function activateField() {
-        updateFieldActive(false);
-    }
-    
-    function onChange(e){
-        e.preventDefault(e);
-        updateInputValue(e.target.value);
-       // activateField(e);
-    }*/
-    
 
     return(
         <div className = 'mds-textfield'>
             <input className = 'mds-textfield__input'
                    type = 'text'
                    value = {inputValue}
-                   required = ''
-                  // onFocus = {activateField}
-                  // onBlur = {disableFocus}
+                   required
                    onChange = {(e)=> updateInputValue(e.target.value)}/>
             <div className = 'mds-textfield__underline'></div>
             <label className = 'mds-floating__label'>Name</label>
